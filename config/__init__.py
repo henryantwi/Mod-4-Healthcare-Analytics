900602b0e7afa9e1dd7b80608e08f3e9f9f373fe
Henry Nana Antwi <henryantwi191@gmail.com>
2026-01-22 00:28:37 +0000

    feat: Add initial database configuration file with connection details.

---
+++ config/__init__.py
@@ -0,0 +1,11 @@
+"""
+Configuration Package - Database and application settings
+"""
+
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 3306,
+    "user": "root",
+    "password": "root_password",
+    "database": "healthcare_db",
+}
